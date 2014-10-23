f3a002e1be6af94095bcd1c851d38eeee08d13af
Max Leuthäuser <max.leuthaeuser@gmail.com>
2014-10-23 12:55:24 +0200

    - added AP oriented dispatch description

---
+++ src/main/scala/internal/DispatchQuery.scala
@@ -0,0 +1,22 @@
+package internal
+
+object DispatchQuery
+{
+  def From(f: Any => Boolean) = new
+    {
+      def To(t: Any => Boolean) = new
+        {
+          def Through(th: Any => Boolean) = new
+            {
+              def Bypassing(b: Any => Boolean): DispatchQuery = new DispatchQuery(f, t, th, b)
+            }
+        }
+    }
+}
+
+class DispatchQuery(
+  from: Any => Boolean,
+  to: Any => Boolean,
+  through: Any => Boolean,
+  bypassing: Any => Boolean
+  )
