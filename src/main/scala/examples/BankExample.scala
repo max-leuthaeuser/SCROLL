774f4c60d7a8af4cea87e968ae63e0087817ff51
Max Leuthäuser <max.leuthaeuser@gmail.com>
2015-02-23 14:58:31 +0100

    - you can now write select like queries for searching for roles with given attributes or method results  - updated examples

--- src/main/scala/examples/BankExample.scala
+++ src/main/scala/examples/BankExample.scala
@@ -87,9 +87,9 @@ object BankExample extends App {
   class Transaction(val amount: CurrencyRepr) extends Context {
     def execute() {
       info("Executing from Player.")
-      // E_? queries for the first role of the provided type it can find in scope.
-      E_?(Source).withDraw(amount)
-      E_?(Target).deposit(amount)
+      // one queries for the first role of the provided type it can find in scope.
+      one(Source).withDraw(amount)
+      one(Target).deposit(amount)
     }
 
     // To make roles that are contained in some Compartment accessible one
--- src/main/scala/internal/Compartment.scala
+++ src/main/scala/internal/Compartment.scala
@@ -8,10 +8,28 @@ import reflect.runtime.universe._
 import scala.collection.immutable.Queue
 import annotations.Role
 import graph.ScalaRoleGraph
-import util.Log.info
 
 // TODO: what happens if the same role is played multiple times from one player?
 trait Compartment {
+
+  implicit class RoleQueryStrategy(name: String) {
+    def matches(on: Any): Boolean = true
+
+    def ==#[T](value: T) = new WithProperty(name, value)
+
+    def ==>[T](value: T) = new WithResult(name, value)
+  }
+
+  case class *() extends RoleQueryStrategy("")
+
+  case class WithProperty[T](name: String, value: T) extends RoleQueryStrategy(name) with ReflectiveHelper {
+    override def matches(on: Any): Boolean = on.propertyOf[T](name) == value
+  }
+
+  case class WithResult[T](name: String, result: T) extends RoleQueryStrategy(name) with ReflectiveHelper {
+    override def matches(on: Any): Boolean = on.resultOf[T](name) == result
+  }
+
   private def isRole(value: Any): Boolean = {
     require(null != value)
     value.getClass.isAnnotationPresent(classOf[Role])
@@ -39,38 +57,40 @@ trait Compartment {
     })
   }
 
-  def E_?[T](any: T): T = {
-    require(null != any)
-    plays.store.nodes.toSeq.find(v => any.getClass == v.value.getClass) match {
-      case Some(role) => role.value.asInstanceOf[T]
-      case None => throw new RuntimeException(s"No player with type '$any' found.")
-    }
-  }
-
-  def A_?[T](any: T): Seq[T] = {
+  def all[T](any: T, matcher: RoleQueryStrategy = *()): Seq[T] = {
     require(null != any)
     plays.store.nodes.toSeq.filter(v => any.getClass == v.value.getClass)
-      .map(_.value.asInstanceOf[T])
+      .map(_.value.asInstanceOf[T]).filter(a => matcher.matches(getCoreFor(a)))
   }
 
+  def all[T](any: T, matcher: () => Boolean): Seq[T] = {
+    require(null != any)
+    plays.store.nodes.toSeq.filter(v => any.getClass == v.value.getClass)
+      .map(_.value.asInstanceOf[T]).filter(_ => matcher())
+  }
+
+  def one[T](any: T, matcher: RoleQueryStrategy = *()): T = all(any, matcher).head
+
+  def one[T](any: T, matcher: () => Boolean): T = all(any, matcher).head
+
   def addPlaysRelation(
-    core: Any,
-    role: Any) {
+                        core: Any,
+                        role: Any) {
     require(isRole(role), "Argument for adding a role must be a role (you maybe want to add the @Role annotation).")
     plays.addBinding(core, role)
   }
 
   def removePlaysRelation(
-    core: Any,
-    role: Any) {
+                           core: Any,
+                           role: Any) {
     require(isRole(role), "Argument for removing a role must be a role (you maybe want to add the @Role annotation).")
     plays.removeBinding(core, role)
   }
 
   def transferRole(
-    coreFrom: Any,
-    coreTo: Any,
-    role: Any) {
+                    coreFrom: Any,
+                    coreTo: Any,
+                    role: Any) {
     require(null != coreFrom)
     require(null != coreTo)
     require(coreFrom != coreTo, "You can not transfer a role from itself.")
@@ -81,9 +101,9 @@ trait Compartment {
   }
 
   def transferRoles(
-    coreFrom: Any,
-    coreTo: Any,
-    roles: Set[Any]) {
+                     coreFrom: Any,
+                     coreTo: Any,
+                     roles: Set[Any]) {
     require(null != roles)
     roles.foreach(transferRole(coreFrom, coreTo, _))
   }
@@ -105,8 +125,8 @@ trait Compartment {
   trait DispatchType {
     // for single method dispatch
     def dispatch[E](
-      on: Any,
-      m: Method): E = {
+                     on: Any,
+                     m: Method): E = {
       require(null != on)
       require(null != m)
       m.invoke(on).asInstanceOf[E]
@@ -114,74 +134,70 @@ trait Compartment {
 
     // for multi-method / multi-argument dispatch
     def dispatch[E, A](
-      on: Any,
-      m: Method,
-      args: Seq[A]): E =
-      {
-        require(null != on)
-        require(null != m)
-        require(null != args)
-        val argTypes: Array[Class[_]] = m.getParameterTypes
-        val actualArgs: Seq[Any] = args.zip(argTypes).map {
-          case (arg: PlayerType[_], tpe: Class[_]) =>
-            plays.getRoles(arg.core).find(_.getClass == tpe) match {
-              case Some(curRole) => curRole
-              // TODO: how to permit this?
-              case None => throw new RuntimeException(s"No role for type '$tpe' found.")
-            }
-          case (arg: Double, tpe: Class[_]) => new lang.Double(arg.toDouble)
-          case (arg @ unchecked, tpe: Class[_]) => tpe.cast(arg)
-        }
-        // that looks funny:
-        m.invoke(on, actualArgs.map {
-          _.asInstanceOf[Object]
-        }: _*).asInstanceOf[E]
+                        on: Any,
+                        m: Method,
+                        args: Seq[A]): E = {
+      require(null != on)
+      require(null != m)
+      require(null != args)
+      val argTypes: Array[Class[_]] = m.getParameterTypes
+      val actualArgs: Seq[Any] = args.zip(argTypes).map {
+        case (arg: PlayerType[_], tpe: Class[_]) =>
+          plays.getRoles(arg.core).find(_.getClass == tpe) match {
+            case Some(curRole) => curRole
+            // TODO: how to permit this?
+            case None => throw new RuntimeException(s"No role for type '$tpe' found.")
+          }
+        case (arg: Double, tpe: Class[_]) => new lang.Double(arg.toDouble)
+        case (arg@unchecked, tpe: Class[_]) => tpe.cast(arg)
       }
+      // that looks funny:
+      m.invoke(on, actualArgs.map {
+        _.asInstanceOf[Object]
+      }: _*).asInstanceOf[E]
+    }
 
     // TODO: this looks quite awful. Re-implement this smelly thing!
     protected def reorder(
-      anys: Queue[Any],
-      dispatchQuery: DispatchQuery): Queue[Any] =
-      {
-        require(null != anys)
-        require(null != dispatchQuery)
-        var result = Queue[Any]()
-        anys.foreach { a =>
-          {
-            if (dispatchQuery.bypassing(a)) {
-              result = result :+ a
-            }
-          }
+                           anys: Queue[Any],
+                           dispatchQuery: DispatchQuery): Queue[Any] = {
+      require(null != anys)
+      require(null != dispatchQuery)
+      var result = Queue[Any]()
+      anys.foreach { a => {
+        if (dispatchQuery.bypassing(a)) {
+          result = result :+ a
         }
-        result
       }
+      }
+      result
+    }
   }
 
   implicit class RoleType[T](val role: T) extends Dynamic with DispatchType {
     def unary_- : RoleType[T] = this
 
-    def applyDynamic[E, A](name: String)(args: A*)(implicit dispatchQuery: DispatchQuery = DispatchQuery.empty): E =
-      {
-        val core = getCoreFor(role)
-        val anys = reorder(Queue() ++ plays.getRoles(core) :+ role :+ core, dispatchQuery)
-        anys.foreach(r => {
-          r.getClass.getDeclaredMethods.find(m => m.getName == name).foreach(fm => {
-            args match {
-              case Nil => return dispatch(r, fm)
-              case _ => return dispatch(r, fm, args.toSeq)
-            }
-          })
+    def applyDynamic[E, A](name: String)(args: A*)(implicit dispatchQuery: DispatchQuery = DispatchQuery.empty): E = {
+      val core = getCoreFor(role)
+      val anys = reorder(Queue() ++ plays.getRoles(core) :+ role :+ core, dispatchQuery)
+      anys.foreach(r => {
+        r.getClass.getDeclaredMethods.find(m => m.getName == name).foreach(fm => {
+          args match {
+            case Nil => return dispatch(r, fm)
+            case _ => return dispatch(r, fm, args.toSeq)
+          }
         })
+      })
 
-        // otherwise give up
-        throw new RuntimeException(s"No role with method '$name' found! (role: '$role')")
-      }
+      // otherwise give up
+      throw new RuntimeException(s"No role with method '$name' found! (role: '$role')")
+    }
 
     def isPlaying[E: WeakTypeTag]: Boolean = plays.getRoles(role)
       .find(r => r.getClass.getSimpleName == ReflectiveHelper.typeSimpleClassName(weakTypeOf[E])) match {
-        case None => false
-        case _ => true
-      }
+      case None => false
+      case _ => true
+    }
 
     // identity of roles: they don't have their own ID
     override def equals(o: Any) = o match {
@@ -194,20 +210,18 @@ trait Compartment {
   }
 
   implicit class PlayerType[T](val core: T) extends Dynamic with DispatchType {
-    def play(role: Any): PlayerType[T] =
-      {
-        core match {
-          case p: PlayerType[_] => addPlaysRelation(p.core, role)
-          case p: Any => addPlaysRelation(p, role)
-        }
-        this
+    def play(role: Any): PlayerType[T] = {
+      core match {
+        case p: PlayerType[_] => addPlaysRelation(p.core, role)
+        case p: Any => addPlaysRelation(p, role)
       }
+      this
+    }
 
-    def drop(role: Any): PlayerType[T] =
-      {
-        removePlaysRelation(core, role)
-        this
-      }
+    def drop(role: Any): PlayerType[T] = {
+      removePlaysRelation(core, role)
+      this
+    }
 
     def unary_+ : PlayerType[T] = this
 
@@ -217,26 +231,25 @@ trait Compartment {
       }
     }
 
-    def applyDynamic[E, A](name: String)(args: A*)(implicit dispatchQuery: DispatchQuery = DispatchQuery.empty): E =
-      {
-        val anys = reorder(Queue() ++ plays.getRoles(getCoreFor(core)).tail :+ core :+ getCoreFor(core), dispatchQuery)
-        anys.foreach(r => {
-          r.getClass.getDeclaredMethods.find(m => m.getName == name).foreach(fm => {
-            args match {
-              case Nil => return dispatch(r, fm)
-              case _ => return dispatch(r, fm, args.toSeq)
-            }
-          })
+    def applyDynamic[E, A](name: String)(args: A*)(implicit dispatchQuery: DispatchQuery = DispatchQuery.empty): E = {
+      val anys = reorder(Queue() ++ plays.getRoles(getCoreFor(core)).tail :+ core :+ getCoreFor(core), dispatchQuery)
+      anys.foreach(r => {
+        r.getClass.getDeclaredMethods.find(m => m.getName == name).foreach(fm => {
+          args match {
+            case Nil => return dispatch(r, fm)
+            case _ => return dispatch(r, fm, args.toSeq)
+          }
         })
-        // otherwise give up
-        throw new RuntimeException(s"No role with method '$name' found! (core: '$core')")
-      }
+      })
+      // otherwise give up
+      throw new RuntimeException(s"No role with method '$name' found! (core: '$core')")
+    }
 
     def isPlaying[E: WeakTypeTag]: Boolean = plays.getRoles(core)
       .find(r => r.getClass.getSimpleName == ReflectiveHelper.typeSimpleClassName(weakTypeOf[E])) match {
-        case None => false
-        case _ => true
-      }
+      case None => false
+      case _ => true
+    }
 
     override def equals(o: Any) = o match {
       case that: PlayerType[_] => that.core == this.core
@@ -245,4 +258,5 @@ trait Compartment {
 
     override def hashCode(): Int = core.hashCode()
   }
+
 }
