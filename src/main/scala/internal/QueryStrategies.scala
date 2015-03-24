df669d2cbff1b9e7944d1db8e6f5e33b7eab3eb8
Max Leuthäuser <max.leuthaeuser@gmail.com>
2015-03-24 14:15:08 +0100

    - added some scala doc

--- src/main/scala/internal/QueryStrategies.scala
+++ src/main/scala/internal/QueryStrategies.scala
@@ -1,11 +1,26 @@
 package internal
 
 trait QueryStrategies extends ReflectiveHelper {
+
   implicit class RoleQueryStrategy(name: String) {
     def matches(on: Any): Boolean = true
 
+    /**
+     * Returns the value the queried attribute.
+     *
+     * @param value the name of the attribute that is queried
+     * @tparam T its type
+     * @return the value of the queried attribute
+     */
     def ==#[T](value: T) = new WithProperty(name, value)
 
+    /**
+     * Returns the return value the queried function.
+     *
+     * @param value the name of the function that is queried
+     * @tparam T its return type
+     * @return the return value of the queried function
+     */
     def ==>[T](value: T) = new WithResult(name, value)
   }
 
@@ -18,4 +33,5 @@ trait QueryStrategies extends ReflectiveHelper {
   case class WithResult[T](name: String, result: T) extends RoleQueryStrategy(name) {
     override def matches(on: Any): Boolean = on.resultOf[T](name) == result
   }
+
 }
