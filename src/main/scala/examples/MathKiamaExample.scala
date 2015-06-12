7f9712f080e11552360ff7044450f986b78a9b2c
Max Leuthäuser <max.leuthaeuser@gmail.com>
2015-06-12 10:45:40 +0200

    -enhanced example a little bit

--- src/main/scala/examples/MathKiamaExample.scala
+++ src/main/scala/examples/MathKiamaExample.scala
@@ -3,24 +3,32 @@ package examples
 import internal.Compartment
 import org.kiama.attribution.Attribution.attr
 import org.kiama.rewriting.Rewriter.{attempt, bottomup, rewrite, rule}
-import org.kiama.util.{ParserUtilities, TreeNode}
+import org.kiama.util.TreeNode
+
+import scala.util.parsing.combinator.JavaTokenParsers
 
 object MathKiamaExample extends App with Compartment {
 
   sealed abstract class Exp extends TreeNode
 
-  case class Num(i: Int) extends Exp
+  case class Num(i: Double) extends Exp
 
   case class Add(l: Exp, r: Exp) extends Exp
 
+  case class Sub(l: Exp, r: Exp) extends Exp
+
   case class Mul(l: Exp, r: Exp) extends Exp
 
+  case class Div(l: Exp, r: Exp) extends Exp
+
   case class SimpleMath() {
-    val value: Exp => Int =
+    val value: Exp => Double =
       attr {
         case Num(i) => i
         case Add(l, r) => (l -> value) + (r -> value)
+        case Sub(l, r) => (l -> value) - (r -> value)
         case Mul(l, r) => (l -> value) * (r -> value)
+        case Div(l, r) => (l -> value) / (r -> value)
       }
   }
 
@@ -34,42 +42,51 @@ object MathKiamaExample extends App with Compartment {
       rule[Exp] {
         case Add(Num(0), e) => e
         case Add(e, Num(0)) => e
+        case Sub(Num(0), e) => e
+        case Sub(e, Num(0)) => e
         case Mul(Num(1), e) => e
         case Mul(e, Num(1)) => e
         case Mul(z@Num(0), _) => z
         case Mul(_, z@Num(0)) => z
+        case Div(e, Num(1)) => e
+        case Div(_, Num(0)) => throw new IllegalArgumentException("Division by 0!")
       }
   }
 
-  case class Parser() extends ParserUtilities {
+  case class Parser() extends JavaTokenParsers {
 
-    def parse(in: String): Exp = parseString(parser, in) match {
-      case Left(a) => a
-      case Right(err) => throw new IllegalArgumentException(in + " is no valid Exp! " + err)
+    def parse(in: String): Exp = parseAll(expr, in).get
+
+    lazy val expr: Parser[Exp] = term ~ rep("[+-]".r ~ term) ^^ {
+      case t ~ ts => ts.foldLeft(t) {
+        case (t1, "+" ~ t2) => Add(t1, t2)
+        case (t1, "-" ~ t2) => Sub(t1, t2)
+      }
     }
 
-    lazy val parser = phrase(exp)
+    lazy val term = factor ~ rep("[*/]".r ~ factor) ^^ {
+      case t ~ ts => ts.foldLeft(t) {
+        case (t1, "*" ~ t2) => Mul(t1, t2)
+        case (t1, "/" ~ t2) => Div(t1, t2)
+      }
+    }
 
-    lazy val exp: PackratParser[Exp] = exp ~ ("+" ~> term) ^^ Add | term
+    lazy val factor = "(" ~> expr <~ ")" | num
 
-    lazy val term: PackratParser[Exp] = term ~ ("*" ~> factor) ^^ Mul | factor
-
-    lazy val factor: PackratParser[Exp] = integer | "(" ~> exp <~ ")"
-
-    lazy val integer = "[0-9]+".r ^^ (s => Num(s.toInt))
+    lazy val num = floatingPointNumber ^^ (s => Num(s.toDouble))
 
   }
 
   // make it run
   val someMath = SimpleMath() play Optimizer() play Parser()
 
-  val ast: Exp = someMath.parse("1+2*3*0")
+  val ast: Exp = someMath.parse("1+2+3*0")
   val optimizedAst: Exp = someMath.optimise(ast)
-  val resultFunc: Exp => Int = someMath.value
+  val resultFunc: Exp => Double = someMath.value
   val result = resultFunc(optimizedAst)
 
   println("AST: " + ast)
   println("optimized AST: " + optimizedAst)
   println("Result: " + result)
-  assert(1 == result)
+  assert(3 == result)
 }
