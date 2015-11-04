1bfb6ff9454b35fd3a64dd29d73e2da8c404cb1e
Max Leuthäuser <max.leuthaeuser@gmail.com>
2015-11-04 18:17:55 +0100

    - prepared RoleGroups for validation

--- core/src/main/scala/scroll/internal/support/RoleGroups.scala
+++ core/src/main/scala/scroll/internal/support/RoleGroups.scala
@@ -10,6 +10,26 @@ trait RoleGroups {
 
   private lazy val roleGroups = mutable.HashMap.empty[String, RoleGroup]
 
+  /**
+    * Wrapping function that checks all available role group constraints for
+    * all core objects and its roles after the given function was executed.
+    * Throws a RuntimeException if a role group constraint is violated!
+    *
+    * @param func the function to execute and check role group constraints afterwards
+    */
+  def RoleGroupsChecked(func: => Unit) {
+    func
+    validate()
+  }
+
+  /**
+    * Checks all role groups.
+    * Will throw a RuntimeException if a role group constraint is violated!
+    */
+  private def validate() {
+    ???
+  }
+
   private def addRoleGroup(rg: RoleGroup): RoleGroup = {
     roleGroups.exists { case (n, _) => n == rg.name } match {
       case true => throw new RuntimeException(s"The RoleGroup ${rg.name} was already added!")
@@ -41,35 +61,37 @@ trait RoleGroups {
   }
 
   object RoleGroup {
+    private implicit def manifest2String(m: Manifest[_]): String = m.toString()
+
     def apply(name: String) = new {
 
       def containing(rg: RoleGroup*)(occ_l: Int, occ_u: Int)(limit_l: Int, limit_u: Int) =
         addRoleGroup(new RoleGroup(name, rg, (occ_l, occ_u), (limit_l, limit_u)))
 
       def containing[T1: Manifest](occ_l: Int, occ_u: Int)(limit_l: Int, limit_u: Int) = {
-        val entry = Types(manifest[T1].toString())
+        val entry = Types(manifest[T1])
         addRoleGroup(new RoleGroup(name, Seq(entry), (occ_l, occ_u), (limit_l, limit_u)))
       }
 
 
       def containing[T1: Manifest, T2: Manifest](occ_l: Int, occ_u: Int)(limit_l: Int, limit_u: Int): RoleGroup = {
-        val entry = Types(manifest[T1].toString(), manifest[T2].toString())
+        val entry = Types(manifest[T1], manifest[T2])
         addRoleGroup(new RoleGroup(name, Seq(entry), (occ_l, occ_u), (limit_l, limit_u)))
       }
 
       def containing[T1: Manifest, T2: Manifest, T3: Manifest](occ_l: Int, occ_u: Int)(limit_l: Int, limit_u: Int): RoleGroup = {
-        val entry = Types(manifest[T1].toString(), manifest[T2].toString(), manifest[T3].toString())
+        val entry = Types(manifest[T1], manifest[T2], manifest[T3])
         addRoleGroup(new RoleGroup(name, Seq(entry), (occ_l, occ_u), (limit_l, limit_u)))
       }
 
       def containing[T1: Manifest, T2: Manifest, T3: Manifest, T4: Manifest](occ_l: Int, occ_u: Int)(limit_l: Int, limit_u: Int): RoleGroup = {
-        val entry = Types(manifest[T1].toString(), manifest[T2].toString(), manifest[T3].toString(), manifest[T4].toString())
+        val entry = Types(manifest[T1], manifest[T2], manifest[T3], manifest[T4])
         addRoleGroup(new RoleGroup(name, Seq(entry), (occ_l, occ_u), (limit_l, limit_u)))
       }
 
 
       def containing[T1: Manifest, T2: Manifest, T3: Manifest, T4: Manifest, T5: Manifest](occ_l: Int, occ_u: Int)(limit_l: Int, limit_u: Int): RoleGroup = {
-        val entry = Types(manifest[T1].toString(), manifest[T2].toString(), manifest[T3].toString(), manifest[T4].toString(), manifest[T5].toString())
+        val entry = Types(manifest[T1], manifest[T2], manifest[T3], manifest[T4], manifest[T5])
         addRoleGroup(new RoleGroup(name, Seq(entry), (occ_l, occ_u), (limit_l, limit_u)))
       }
 
