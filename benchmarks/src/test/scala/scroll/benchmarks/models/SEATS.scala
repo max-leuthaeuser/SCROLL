eece43aa69da9312c3edc96ba44d4aede2add407
Max Leuthäuser <max.leuthaeuser@gmail.com>
2015-11-05 10:04:12 +0100

    - fixed missing Many in remaining file

--- benchmarks/src/test/scala/scroll/benchmarks/models/SEATS.scala
+++ benchmarks/src/test/scala/scroll/benchmarks/models/SEATS.scala
@@ -2,6 +2,7 @@ package scroll.benchmarks.models
 
 import scroll.internal.annotations.Role
 import scroll.internal.Compartment
+import scroll.internal.util.Many._
 
 object SEATS {
 
@@ -20,11 +21,11 @@ object SEATS {
     import Relationship._
 
     val distance_between = Relationship("distance_between").from[ArrivalAirport](1).to[DepartureAirport](1)
-    val flying_to = Relationship("flying_to").from[PlaneInComission](0 To Many()).to[DepartureAirport](1)
-    val depart_from = Relationship("depart_from").from[PlaneInComission](0 To Many()).to[DepartureAirport](1)
-    val owns = Relationship("owns").from[Airline](1).to[PlaneInComission](1 To Many())
-    val reserves = Relationship("reserves").from[Customer](1 To Many()).to[PlaneInComission](1 To Many())
-    val is_frequent_flyer = Relationship("is_frequent_flyer").from[Customer](0 To Many()).to[Airline](0 To 1)
+    val flying_to = Relationship("flying_to").from[PlaneInComission](0 To *).to[DepartureAirport](1)
+    val depart_from = Relationship("depart_from").from[PlaneInComission](0 To *).to[DepartureAirport](1)
+    val owns = Relationship("owns").from[Airline](1).to[PlaneInComission](1 To *)
+    val reserves = Relationship("reserves").from[Customer](1 To *).to[PlaneInComission](1 To *)
+    val is_frequent_flyer = Relationship("is_frequent_flyer").from[Customer](0 To *).to[Airline](0 To 1)
 
     @Role
     class Airline
