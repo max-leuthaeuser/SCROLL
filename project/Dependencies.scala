818abd8b63d4ccdd78fa8cca5c5b1818c47eeb5c
Max Leuthäuser <max.leuthaeuser@itemis.de>
2018-12-11 13:57:50 +0100

    - updated dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ trait Dependencies {
 
   object v {
     val scalaVersion = "2.12.8"
-    val akkaVersion = "2.5.18"
+    val akkaVersion = "2.5.19"
     val shapelessVersion = "2.3.3"
     val scalatestVersion = "3.0.5"
     val chocoVersion = "4.0.9"
