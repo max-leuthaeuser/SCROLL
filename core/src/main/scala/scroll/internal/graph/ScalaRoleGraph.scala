9dc712be08957be9b09dc963611e8f573a6ff796
Max Leuthäuser <max.leuthaeuser@itemis.de>
2019-02-04 11:53:02 +0100

    - removed/refactored duplicated code

--- core/src/main/scala/scroll/internal/graph/ScalaRoleGraph.scala
+++ core/src/main/scala/scroll/internal/graph/ScalaRoleGraph.scala
@@ -21,11 +21,9 @@ class ScalaRoleGraph(checkForCycles: Boolean = true) extends RoleGraph {
   private val root: MutableGraph[Object] = GraphBuilder.directed().build[Object]()
 
   override def addPart(other: RoleGraph): Boolean = {
-    require(null != other)
     require(other.isInstanceOf[ScalaRoleGraph], MERGE_MESSAGE)
 
     val target = other.asInstanceOf[ScalaRoleGraph].root
-
     if (!target.nodes().isEmpty) {
       target.edges().forEach(p => {
         val _ = root.putEdge(p.source(), p.target())
@@ -61,70 +59,49 @@ class ScalaRoleGraph(checkForCycles: Boolean = true) extends RoleGraph {
   }
 
   override def removeBinding[P <: AnyRef : ClassTag, R <: AnyRef : ClassTag](player: P, role: R): Unit = {
-    require(null != player)
-    require(null != role)
     val _ = root.removeEdge(player, role)
   }
 
   override def removePlayer[P <: AnyRef : ClassTag](player: P): Unit = {
-    require(null != player)
     val _ = root.removeNode(player)
   }
 
-  override def roles(player: AnyRef): Seq[AnyRef] = {
-    require(null != player)
-    if (containsPlayer(player)) {
-      val returnSeq = new mutable.ListBuffer[Object]
-      val processing = new mutable.Queue[Object]
-      val _ = returnSeq += player.asInstanceOf[Object]
-      root.successors(player.asInstanceOf[Object]).forEach(n => if (!n.isInstanceOf[Enumeration#Value]) processing.enqueue(n))
-      while (processing.nonEmpty) {
-        val next = processing.dequeue()
-        if (!returnSeq.contains(next)) {
-          val _ = returnSeq += next
-        }
-        root.successors(next).forEach(n => if (!n.isInstanceOf[Enumeration#Value]) processing.enqueue(n))
+  private def follow(player: AnyRef, direction: Object => java.util.Set[Object]): Seq[AnyRef] = {
+    val returnSeq = new mutable.ListBuffer[Object]
+    val processing = new mutable.Queue[Object]
+    direction(player).forEach(n => if (!n.isInstanceOf[Enumeration#Value]) processing.enqueue(n))
+    while (processing.nonEmpty) {
+      val next = processing.dequeue()
+      if (!returnSeq.contains(next)) {
+        val _ = returnSeq += next
       }
-      returnSeq
+      direction(next).forEach(n => if (!n.isInstanceOf[Enumeration#Value]) processing.enqueue(n))
+    }
+    returnSeq
+  }
+
+  override def roles(player: AnyRef): Seq[AnyRef] =
+    if (containsPlayer(player)) {
+      player +: follow(player, root.successors)
     } else {
       Seq.empty[AnyRef]
     }
-  }
 
-  override def facets(player: AnyRef): Seq[Enumeration#Value] = {
-    require(null != player)
+  override def facets(player: AnyRef): Seq[Enumeration#Value] =
     if (containsPlayer(player)) {
-      val returnSeq = new mutable.ListBuffer[Enumeration#Value]
-      root.successors(player.asInstanceOf[Object]).forEach {
-        case e: Enumeration#Value => val _ = returnSeq += e
-        case _ =>
-      }
-      returnSeq
+      root.successors(player).asScala.toSeq.collect { case v: Enumeration#Value => v }
     } else {
       Seq.empty[Enumeration#Value]
     }
-  }
 
   override def containsPlayer(player: AnyRef): Boolean = root.nodes().contains(player)
 
   override def allPlayers: Seq[AnyRef] = root.nodes().asScala.toSeq
 
-  override def predecessors(player: AnyRef): Seq[AnyRef] = {
-    require(null != player)
+  override def predecessors(player: AnyRef): Seq[AnyRef] =
     if (containsPlayer(player)) {
-      val returnSeq = new mutable.ListBuffer[Object]
-      val processing = new mutable.Queue[Object]
-      root.predecessors(player.asInstanceOf[Object]).forEach(n => if (!n.isInstanceOf[Enumeration#Value]) processing.enqueue(n))
-      while (processing.nonEmpty) {
-        val next = processing.dequeue()
-        if (!returnSeq.contains(next)) {
-          val _ = returnSeq += next
-        }
-        root.predecessors(next).forEach(n => if (!n.isInstanceOf[Enumeration#Value]) processing.enqueue(n))
-      }
-      returnSeq
+      follow(player, root.predecessors)
     } else {
       Seq.empty[AnyRef]
     }
-  }
 }
