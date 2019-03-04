2c1adc6d885d04a11458bf6164bfef8db124e415
Max Leuthäuser <max.leuthaeuser@itemis.de>
2019-03-04 10:55:56 +0100

    - updated dependency to Scalatest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ trait Dependencies {
     val scalaVersion = "2.12.8"
     val akkaVersion = "2.5.21"
     val shapelessVersion = "2.3.3"
-    val scalatestVersion = "3.0.5"
+    val scalatestVersion = "3.0.6"
     val chocoVersion = "4.10.0"
     val guavaVersion = "27.0.1-jre"
     val emfcommonVersion = "2.15.0"
