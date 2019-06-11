ca9c109282a9422f8c79559661563bdb7505051b
Max Leuthäuser <max.leuthaeuser@itemis.de>
2019-06-11 12:00:53 +0200

    - replaced .size on potentially large collection with new .sizeIs
    Signed-off-by: Max Leuthäuser <max.leuthaeuser@itemis.de>

--- core/src/main/scala/scroll/internal/support/PlayerEquality.scala
+++ core/src/main/scala/scroll/internal/support/PlayerEquality.scala
@@ -10,9 +10,9 @@ trait PlayerEquality {
   def equalsPlayer[W <: AnyRef : ClassTag](a: IPlayer[W, _], b: IPlayer[W, _]): Boolean = {
     val coreA = coreFor(a.wrapped)
     val coreB = coreFor(b.wrapped)
-    if (coreA.size == 1) {
+    if (coreA.sizeIs == 1) {
       coreA.headOption.exists(coreB.lastOption.contains)
-    } else if (coreB.size == 1) {
+    } else if (coreB.sizeIs == 1) {
       coreB.headOption.exists(coreA.lastOption.contains)
     } else {
       coreA == coreB
@@ -21,7 +21,7 @@ trait PlayerEquality {
 
   def equalsAny[W <: AnyRef : ClassTag](a: IPlayer[W, _], b: Any): Boolean = {
     val coreA = coreFor(a.wrapped)
-    if (coreA.size == 1) {
+    if (coreA.sizeIs == 1) {
       coreA.headOption.contains(b)
     } else {
       coreA.lastOption.contains(b)
