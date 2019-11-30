29899949dff5007153a73de792ff14652938cf38
Scala Steward <me@scala-steward.org>
2019-11-30 20:56:54 +0100

    Update scalatest, scalatest-core, ... to 3.2.0-M2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ trait Dependencies {
     val scalaVersion = "2.13.0"
     val akkaVersion = "2.6.0"
     val shapelessVersion = "2.3.3"
-    val scalatestVersion = "3.2.0-M1"
+    val scalatestVersion = "3.2.0-M2"
     val chocoVersion = "4.10.2"
     val guavaVersion = "28.1-jre"
     val emfcommonVersion = "2.16.0"
