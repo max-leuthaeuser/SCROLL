bf582579129f63742f672ecc42fcd5bce6ffd12d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-06-15 06:30:44 +0200

    Update org.eclipse.emf.ecore to 2.22.0 (#103)
    Co-authored-by: Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,8 +11,8 @@ trait Dependencies {
     val scalatestVersion = "3.2.0-M4"
     val chocoVersion = "4.10.2"
     val guavaVersion = "29.0-jre"
+    val emfecoreVersion = "2.22.0"
     val emfcommonVersion = "2.19.0"
-    val emfecoreVersion = "2.21.0"
     val umlVersion = "3.1.0.v201006071150"
     val jvm = "1.8"
   }
