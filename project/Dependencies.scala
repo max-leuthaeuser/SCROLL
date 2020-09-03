086533d47f7e3600f58e9959de5e9bb7d26ef0db
Scala Steward <me@scala-steward.org>
2020-09-03 22:41:30 +0200

    Update org.eclipse.emf.common to 2.20.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ trait Dependencies {
     val chocoVersion = "4.10.2"
     val guavaVersion = "29.0-jre"
     val emfecoreVersion = "2.22.0"
-    val emfcommonVersion = "2.19.0"
+    val emfcommonVersion = "2.20.0"
     val umlVersion = "3.1.0.v201006071150"
     val jvm = "1.8"
   }
