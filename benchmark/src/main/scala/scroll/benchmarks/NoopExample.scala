a35e74cd2479d5945a682d61b89c75bfcc7eb67c
Max Leuthäuser <max.leuthaeuser@gmail.de>
2020-12-21 14:24:15 +0100

    Small fix to get sub-project benchmark running with Scala 3

--- benchmark/src/main/scala/scroll/benchmarks/NoopExample.scala
+++ benchmark/src/main/scala/scroll/benchmarks/NoopExample.scala
@@ -6,6 +6,8 @@ import scroll.internal.dispatch.DispatchQuery._
 
 class NoopExample(cached: Boolean) {
 
+  val compartment = new NoopCompartment()
+
   class BaseType {
     def noArgs(): AnyRef = this
 
@@ -17,8 +19,10 @@ class NoopExample(cached: Boolean) {
     def primitiveArgsAndReturn(x: Int, y: Int): Int = x + y
   }
 
-  trait NoopCompartment extends Compartment {
+  class NoopCompartment extends Compartment {
     roleGraph.reconfigure(cached = cached, checkForCycles = false)
+
+    val player = new BaseType() play new NoopRole()
 
     /**
       * No-op role methods which just forward to the base
@@ -31,7 +35,7 @@ class NoopExample(cached: Boolean) {
       }
 
       def referenceArgAndReturn(o: AnyRef): AnyRef = {
-        (+this).referenceArgAndReturn o
+        (+this).referenceArgAndReturn(o)
       }
 
       def primitiveArgsAndReturn(x: Int, y: Int): Int = {
@@ -41,7 +45,4 @@ class NoopExample(cached: Boolean) {
 
   }
 
-  val compartment = new NoopCompartment {
-    val player = new BaseType() play new NoopRole()
-  }
 }
