c3e79200f13be00155efc7e24bdb6d3726e3f5c0
Max Leuthäuser <max.leuthaeuser@gmail.de>
2021-02-22 13:48:40 +0100

    Downgraded scalatest dependencies; not available for dotty yet.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ trait Dependencies {
   object v {
     val scalaVersion     = "3.0.0-M2"
     val akkaVersion      = "2.6.12"
-    val scalatestVersion = "3.2.5"
+    val scalatestVersion = "3.2.3"
     val chocoVersion     = "4.10.6"
     val guavaVersion     = "30.1-jre"
     val emfcommonVersion = "2.21.0"
