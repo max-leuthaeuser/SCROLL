591e25d7098c58bd4dd14e7a155ed36393009fb1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-04-07 20:47:54 +0200

    Update akka-actor to 2.6.14 (#161)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ trait Dependencies {
 
   object v {
     val scalaVersion = "2.13.5"
-    val akkaVersion = "2.6.13"
+    val akkaVersion = "2.6.14"
     val shapelessVersion = "2.3.3"
     val scalatestVersion = "3.2.7"
     val chocoVersion = "4.10.6"
