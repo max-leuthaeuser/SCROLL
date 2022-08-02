2bc09520104d9cae6f8f966c2a6adadcdb08b66d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-02 17:48:31 +0200

    Update scalatest, scalatest-core, ... to 3.2.13 (#242)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ trait Dependencies {
   object v {
     val scalaVersion     = "3.1.3"
     val akkaVersion      = "2.6.19"
-    val scalatestVersion = "3.2.12"
+    val scalatestVersion = "3.2.13"
     val chocoVersion     = "4.10.8"
     val guavaVersion     = "31.1-jre"
     val emfcommonVersion = "2.25.0"
