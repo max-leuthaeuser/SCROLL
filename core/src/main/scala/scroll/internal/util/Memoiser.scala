509fcf673fc276e15a50557b301fa2fd6f2aac84
Scala Steward <scala_steward@virtuslab.com>
2023-10-24 21:33:58 +0000

    Reformat with scalafmt 3.7.15
    Executed command: scalafmt --non-interactive

--- core/src/main/scala/scroll/internal/util/Memoiser.scala
+++ core/src/main/scala/scroll/internal/util/Memoiser.scala
@@ -9,14 +9,14 @@ import com.google.common.cache.LoadingCache
 object Memoiser {
 
   /** Builds a cache, which either returns an already-loaded value for a given key or atomically computes or retrieves
-    * it using the supplied { @code supplier }. If another thread is currently loading the value for this key, simply
+    * it using the supplied {@@codesupplier} . If another thread is currently loading the value for this key, simply
     * waits for that thread to finish and returns its loaded value. Note that multiple threads can concurrently load
     * values for distinct keys.
     *
     * @param supplier
-    *   the function to be used for loading values; must never return { @code null}
+    *   the function to be used for loading values; must never return {@@codenull}
     * @return
-    *   a cache loader that loads values by passing each key to { @code supplier}
+    *   a cache loader that loads values by passing each key to {@@codesupplier}
     */
   def buildCache[K <: AnyRef, V <: AnyRef](supplier: K => V): LoadingCache[K, V] =
     CacheBuilder.newBuilder().build[K, V](CacheLoader.from((k: K) => supplier(k)))
