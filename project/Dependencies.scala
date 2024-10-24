5215983538aa9de33e18393c38b54b981d10304b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-10-24 21:24:27 +0200

    Update scala3-library to 3.5.2 (#332)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies extends Dependencies
 trait Dependencies {
 
   object v {
-    val scalaVersion     = "3.5.1"
+    val scalaVersion     = "3.5.2"
     val akkaVersion      = "2.8.6"
     val scalatestVersion = "3.2.19"
     val chocoVersion     = "4.10.17"
