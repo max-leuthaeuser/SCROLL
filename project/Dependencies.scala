c21999b5e6776606d07e7c715345f6fa491cd1d1
Scala Steward <scala_steward@virtuslab.com>
2026-04-15 17:44:09 +0000

    Update guava to 33.6.0-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ trait Dependencies {
     val zioVersion       = "2.1.25"
     val scalatestVersion = "3.2.20"
     val chocoVersion     = "5.0.1"
-    val guavaVersion     = "33.5.0-jre"
+    val guavaVersion     = "33.6.0-jre"
     val emfcommonVersion = "2.45.0"
     val emfecoreVersion  = "2.42.0"
     val umlVersion       = "3.1.0.v201006071150"
